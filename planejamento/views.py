e599e46c45d90492599a6264eb9588dd9ded7d79
Rodrigo <rodrigoml2395@gmail.com>
2023-07-05 19:56:14 -0300

    feat: save category

--- planejamento/views.py
+++ planejamento/views.py
@@ -5,6 +5,6 @@ from perfil.models import Category
 # Create your views here.
 
 
-def definir_planejamento(request):
+def define_planning(request):
     categories = Category.objects.all()
     return render(request, "definir_planejamento.html", {"categories": categories})
