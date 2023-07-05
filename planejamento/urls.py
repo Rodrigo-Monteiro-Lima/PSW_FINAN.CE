dcb85a146a655da789803f3ca687d7c9bd323b96
Rodrigo <rodrigoml2395@gmail.com>
2023-07-05 20:11:22 -0300

    feat: show plannig

--- planejamento/urls.py
+++ planejamento/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path("definir_planejamento/", views.define_planning, name="definir_planejamento"),
+    path(
+        "update_valor_categoria/<int:id>",
+        views.update_category_value,
+        name="update_valor_categoria",
+    ),
+    path("ver_planejamento/", views.show_planning, name="ver_planejamento"),
 ]
--- planejamento/views.py
+++ planejamento/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
 from perfil.models import Category
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+
+@csrf_exempt
+def update_category_value(request, id):
+    new_value = json.load(request)["new_value"]
+    category = Category.objects.get(id=id)
+    category.planning_value = new_value
+    category.save()
+
+    return JsonResponse({"status": "Sucesso"})
+
+
+def show_planning(request):
+    categories = Category.objects.all()
+    return render(request, "ver_planejamento.html", {"categories": categories})
 
 
 def define_planning(request):
