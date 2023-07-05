eb4d85a7561a2c4452865530d293c8795a74843d
Rodrigo <rodrigoml2395@gmail.com>
2023-07-05 19:06:12 -0300

    feat: partial statement

--- extrato/urls.py
+++ extrato/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("novo_valor/", views.new_value, name="novo_valor"),
     path("view_extrato/", views.view_statement, name="view_extrato"),
+    path("exportar_pdf/", views.export_pdf, name="exportar_pdf"),
 ]
