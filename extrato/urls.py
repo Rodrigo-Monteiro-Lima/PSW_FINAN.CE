3709aa7807ea5f1a9ff2665081cc4c86c78393ad
Rodrigo <rodrigoml2395@gmail.com>
2023-07-05 18:05:30 -0300

    feat: view statement

--- extrato/urls.py
+++ extrato/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("novo_valor/", views.new_value, name="novo_valor"),
+    path("view_extrato/", views.view_statement, name="view_extrato"),
 ]
--- extrato/views.py
+++ extrato/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-
+from datetime import datetime
+from django.shortcuts import redirect, render
+from extrato.models import Values
 from perfil.models import Account, Category
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 
@@ -12,3 +15,53 @@ def new_value(request):
         return render(
             request, "novo_valor.html", {"accounts": accounts, "categories": categories}
         )
+    elif request.method == "POST":
+        value = request.POST.get("value")
+        category_id = request.POST.get("category")
+        description = request.POST.get("description")
+        date = request.POST.get("date")
+        account_id = request.POST.get("account")
+        value_type = request.POST.get("value_type")
+
+        values = Values(
+            value=value,
+            category_id=category_id,
+            description=description,
+            date=date,
+            account_id=account_id,
+            value_type=value_type,
+        )
+
+        values.save()
+
+        account = Account.objects.get(id=account_id)
+
+        if value_type == "I":
+            account.value += int(value)
+        else:
+            account.value -= int(value)
+
+        account.save()
+
+        if value_type == "I":
+            messages.add_message(
+                request, constants.SUCCESS, "Entrada adicionada com sucesso"
+            )
+        else:
+            messages.add_message(
+                request, constants.SUCCESS, "Saída adicionada com sucesso"
+            )
+        return redirect("/extrato/novo_valor")
+
+
+def views_statement(request):
+    accounts = Account.objects.all()
+    categories = Category.objects.all()
+
+    values = Values.objects.filter(date__month=datetime.now().month)
+
+    return render(
+        request,
+        "view_extrato.html",
+        {"values": values, "accounts": accounts, "categories": categories},
+    )
