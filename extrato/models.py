9e1a61dc66739b6b4f18ac550790599488a5a39b
Rodrigo <rodrigoml2395@gmail.com>
2023-07-05 11:51:49 -0300

    feat: values model

--- extrato/models.py
+++ extrato/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from perfil.models import Category, Account
+
 # Create your models here.
+
+
+class Values(models.Model):
+    choice_type = (("I", "Income"), ("O", "Outcome"))
+
+    value = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    description = models.TextField()
+    date = models.DateField()
+    account = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    value_type = models.CharField(max_length=1, choices=choice_type)
+
+    def __str__(self):
+        return self.description
--- extrato/views.py
+++ extrato/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
+from perfil.models import Account, Category
+
 # Create your views here.
 
 
 def new_value(request):
     if request.method == "GET":
-        contas = Conta.objects.all()
-        categorias = Categoria.objects.all()
+        accounts = Account.objects.all()
+        categories = Category.objects.all()
         return render(
-            request, "novo_valor.html", {"contas": contas, "categorias": categorias}
+            request, "novo_valor.html", {"accounts": accounts, "categories": categories}
         )
