d73a0e452d0045fbf56ab2936fc11477a4d65542
Rodrigo <rodrigoml2395@gmail.com>
2023-07-07 14:02:24 -0300

    feat: total spent

--- planejamento/views.py
+++ planejamento/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from perfil.models import Category
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from perfil.utils import sum_total_value
 import json
 
 
@@ -17,7 +18,21 @@ def update_category_value(request, id):
 
 def show_planning(request):
     categories = Category.objects.all()
-    return render(request, "ver_planejamento.html", {"categories": categories})
+    total_spent = 0
+    for category in categories:
+        total_spent += category.total_spent()
+    total_planned = sum_total_value(categories, "planning_value")
+    percentage_spent = int((total_spent * 100) / total_planned)
+    return render(
+        request,
+        "ver_planejamento.html",
+        {
+            "categories": categories,
+            "total_spent": total_spent,
+            "total_planned": total_planned,
+            "percentage_spent": percentage_spent,
+        },
+    )
 
 
 def define_planning(request):
