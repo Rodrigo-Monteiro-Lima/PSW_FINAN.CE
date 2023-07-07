b32a7b81aaa3a8fedee272e897462f2452cdc2eb
Rodrigo <rodrigoml2395@gmail.com>
2023-07-07 14:25:19 -0300

    feat: bill models

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from perfil.models import Category
+
 # Create your models here.
+
+
+class BillToPay(models.Model):
+    title = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    description = models.TextField()
+    value = models.FloatField()
+    payment_day = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class PaidBill(models.Model):
+    Account = models.ForeignKey(BillToPay, on_delete=models.DO_NOTHING)
+    payment_date = models.DateField()
--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from perfil.models import Category
+from django.contrib import messages
+from django.contrib.messages import constants
+from .models import BillToPay
 
 # Create your views here.
 
@@ -9,3 +12,22 @@ def define_accounts(request):
     if request.method == "GET":
         categories = Category.objects.all()
         return render(request, "definir_contas.html", {"categories": categories})
+    else:
+        title = request.POST.get("title")
+        category = request.POST.get("category")
+        description = request.POST.get("description")
+        value = request.POST.get("value")
+        payment_day = request.POST.get("payment_day")
+
+        bill = BillToPay(
+            title=title,
+            category_id=category,
+            description=description,
+            value=value,
+            payment_day=payment_day,
+        )
+
+        bill.save()
+
+        messages.add_message(request, constants.SUCCESS, "Conta cadastrada com sucesso")
+        return redirect("/contas/definir_contas")
